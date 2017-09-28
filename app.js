7c889e26180dae995ac6821be7a3ad58c8b647fc
Jose Marie(Jey-em) Esplana <jmesplana@gmail.com>
2017-09-28 16:15:53 -0400

    Localhost: 4000

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD");
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(4000, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:4000/');
